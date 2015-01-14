ffa2ae56a899d5f5dbb3ef78b75880643e8a8bb3
Aphentik <Antonin@pc34.home>
2015-01-14 21:37:11 +0100

    i2c alleviation and code cleaning
    Add conditions for less useless i2c communication and some code cleaning

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-    // NEVER use a Sync function except at start-up!
+// NEVER use a Sync function except at start-up!
 var index = fs.readFileSync(__dirname + '/index.html');
 var express = require('express');
 var app = express();
@@ -9,34 +9,35 @@ var server = app.listen(port);
 var url = require('url');
 var querystring = require('querystring');
 var exec = require('child_process').exec;
-var python =true;
 // var i2c = require('i2c');
 // var address = 0x07;
 // var wire = new i2c(address, {device: '/dev/i2c-1'});
+var offset = 40;
 
-// Set "Public" 
+// Set "Public" as root folder for static content
 app.use(express.static(__dirname + '/public'));  
 
-// Chargement de socket.io
+// Socket.io loading
 var io = require('socket.io').listen(server);
 
 // ROUTES
-	app.get('/', function(req, res) {
-    	res.writeHead(200, {'Content-Type': 'text/html'});
-    	res.end(index);	
-	});
+app.get('/', function(req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);	
+});
 
-	app.get('/action', function(req, res) {
-		var params = querystring.parse(url.parse(req.url).query);	//Get URL params	
-		exec('python i2c.py',+ params[' X'] + params[' Y']);		// Execute Python Script with X&Y params
-		console.log(params['X'] + params['Y']);						// Print Params
-		res.writeHead(200, {'Content-Type': 'text/html'});
-    	res.end(index);	
-	});
+app.get('/action', function(req, res) {
+	var params = querystring.parse(url.parse(req.url).query);	//Get URL params	
+	exec('python i2c.py',+ params[' X'] + params[' Y']);		// Execute Python Script with X&Y params
+	console.log(params['X'] + params['Y']);						// Print Params
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);	
+});
 
 // Lors de la connection d'un client 
 io.sockets.on('connection', function (socket) {
     socket.emit('message', 'Vous êtes bien connecté !');
+    console.log("Nouveau client connecté"+ socket.handshake.address);
 
     // Quand le serveur reçoit un signal de type "Coordinate" du client    
     socket.on('coordinate', function(data){
@@ -48,45 +49,39 @@ io.sockets.on('connection', function (socket) {
         console.log('LEFT Coordinate: X = ' + DXL + '; Y = '+DYL);
         console.log('RIGHT Coordinate: X = ' + DXR + '; Y = '+DYR);
 
-        if(python == true){
-            exec('python i2c.py ',+ DXL +' '+ DYL);      // Execute Python Script with X&Y paramsx 
+        var motorRForward,
+            motorRBackward,
+            motorLForward,
+            motorLBackward,
+            breakmotor;
+        var V=(100-Math.abs(DXL))*(DYL/100)+DYL;
+        var W=(100-Math.abs(DYL))*(DXL/100)+DXL;
+        var R= parseInt((V+W)/2);
+        var L=parseInt((V-W)/2);
+        if(R>0){
+            motorRForward = R *2 +offset;
+            motorRBackward = 0;
+        }else if(R<0){
+            motorRBackward = -(R *2)- offset;
+            motorRForward = 0;
         }else{
-            var motorRForward,
-                motorRBackward,
-                motorLForward,
-                motorLBackward,
-                breakmotor;
-            var V=(100-abs(DXL))*(DYL/100)+DYL;
-            var W=(100-abs(DYL))*(DXL/100)+DXL;
-            var R=int((V+W)/2);
-            var L=int((V-W)/2);
-
-            if(R>0){
-                motorRForward = R *2+50;
-                motorRBackward = 0;
-            }else if(R<0){
-                motorRBackward = -(R *2)-50;
-                motorRForward = 0;
-            }else{
-                motorRForward = 0;
-                motorRBackward = 0;
-            }  
-            if(L>0){
-                motorLForward = L *2 +50;
-                motorLBackward = 0;
-            }else if(L<0) {
-                motorLBackward = -L*2-50;
-                motorLForward = 0;
-            }else{
-                motorLForward = 0;
-                motorLBackward = 0;
-            }
-            breakmotor = 0;
-            wire.writeBytes(0x0F, [motorRForward, motorRBackward,motorLForward,motorLBackward,breakmotor], function(err) {});   
-        }      
+            motorRForward = 0;
+            motorRBackward = 0;
+        }  
+        if(L>0){
+            motorLForward = L *2 + offset;
+            motorLBackward = 0;
+        }else if(L<0) {
+            motorLBackward = -L*2- offset;
+            motorLForward = 0;
+        }else{
+            motorLForward = 0;
+            motorLBackward = 0;
+        }
+        breakmotor = 0;
+        //wire.writeBytes(0x0F, [motorRForward, motorRBackward,motorLForward,motorLBackward,breakmotor], function(err) {});        
     });
 });
-
 
 // Will print stacktrace
 if (app.get('env') === 'development') {
