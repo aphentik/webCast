e5dfca281e0fdaa115fe9ad2a6dd616c3a2d6e66
danidano <daniel.sala@icloud.com>
2015-01-22 13:49:57 +0000

    test commit 2

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 var http = require('http');
 var fs = require('fs');
-var nconf = require('nconf');
-  nconf.use('file', { file: './config.json' });
-
 // NEVER use a Sync function except at start-up!
-//var index = fs.readFileSync(__dirname + '/index.html');
+var index = fs.readFileSync(__dirname + '/index.html');
 var express = require('express');
 var app = express();
-    app.engine('html', require('ejs').renderFile);
-    app.set('view engine', 'ejs');
 // Server variables
 var port= 3000;
 var server = app.listen(port);
@@ -21,63 +16,21 @@ var TRex = new i2c(address, {device: '/dev/i2c-1',debug: false});
 var offset = 0;
 var coeff=1;
 
-//Load configuration  
-nconf.load(function (err) {
-if (err) {
-  console.error(err.message);
-  return;
-}
-console.log('Configuration loaded successfully.');
-});
-
 // Set "Public" as root folder for static content
 app.use(express.static(__dirname + '/public'));  
 
 // Socket.io loading
 var io = require('socket.io').listen(server);
 
-
-//nconf.set('acc_mode', 'false');
-//nconf.set('control_mode', 'tank');
-//test
-
 // ROUTES
 app.get('/', function(req, res) {
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-
-    var control_md=nconf.get('control_mode');
-    var acc_md=nconf.get('acc_mode');
-    res.render('index.ejs',{ 
-    control_mode: control_md,
-    acc_mode: acc_md
-  }); 
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end(index);	
 });
-
-app.get('/settings', function(req, res) {
-    //res.writeHead(200, {'Content-Type': 'text/html'});
-
-    var control_md=nconf.get('control_mode');
-    var acc_md=nconf.get('acc_mode');
-    res.render('settings.ejs',{ 
-    control_mode: control_md,
-    acc_mode: acc_md
-  }); 
-});
-
-
-
-nconf.save(function (err) {
-if (err) {
-  console.error(err.message);
-  return;
-}
-console.log('Configuration saved successfully.');
-});
-//console.log(nconf.get('acc_mode'));
 
 // Lors de la connection d'un client 
 io.sockets.on('connection', function (socket) {
-    console.log('New client connected: '+ socket.handshake.address);
+    console.log('New connected client: '+ socket.handshake.address);
 
     // Quand le serveur reçoit un signal de type "Coordinate" du client    
     socket.on('coordinate', function(data){
@@ -94,63 +47,56 @@ io.sockets.on('connection', function (socket) {
             motorRBackward,
             motorLForward,
             motorLBackward,
-            breakmotor,
-        R=0,
-        L=0;
+            breakmotor;
 
         var radius = Math.sqrt(DXL*DXL + DYL*DYL);
         var theta = 2* Math.atan(DYL/(DXL+ radius));
         var pi = Math.PI;
 
-        if(-pi<=theta && theta<-(pi/2))
+        if(-pi<=theta<-(pi/2))
         {
-            R = -radius;
-            L = -radius*(2*theta/pi + 2);
+        	R = -radius;
+        	L = -radius*(2*theta/pi - pi);
         }
-        else if(-pi/2<=theta && theta<0)
+        else if(-pi/2<=theta<0)
         {
-            R = radius*(2*theta/pi);
-            L = -radius;
+        	R = -radius(2*theta/pi);
+        	L = -radius;
         }
-        else if(0<=theta && theta<pi/2)
+        else if(0<=theta<pi/2)
         {
-            R = radius*2*theta/pi;
-            L = radius;
+        	R = radius*2*theta/pi;
+        	L = radius;
         }
-        else if(pi/2<=theta && theta<=pi)
+        else if(pi/2<=theta<=pi)
         {
-            R = radius;
-            L = radius* (-2*theta/pi + 2);
+        	R = radius;
+        	L = radius *(2*theta/pi + pi);
         }
-        //  R=parseInt(1/75000*Math.pow(R,3)+1/500*Math.pow(R,2)+1/15*R)
-        //  L =parseInt(L);
 
-            if(R>0){
-        R=parseInt(1/75000*Math.pow(R,3)+1/500*Math.pow(R,2)+1/15*R)
+
+        if(R>0){
             motorRForward = R *coeff +offset;
             motorRBackward = 0;
         }else if(R<0){
-            R=-parseInt(1/75000*Math.pow(-R,3)+1/500*Math.pow(-R,2)+1/15*-R)
-        motorRBackward = -(R *coeff)+ offset;
+            motorRBackward = -(R *coeff)+ offset;
             motorRForward = 0;
         }else{
             motorRForward = 0;
             motorRBackward = 0;
         }  
         if(L>0){
-        L=parseInt(1/75000*Math.pow(L,3)+1/500*Math.pow(L,2)+1/15*L)
             motorLForward = L *coeff + offset;
             motorLBackward = 0;
         }else if(L<0) {
-        L=-parseInt(1/75000*Math.pow(-L,3)+1/500*Math.pow(-L,2)+1/15*-L)            
-        motorLBackward = -L*coeff+ offset;
+            motorLBackward = -L*coeff+ offset;
             motorLForward = 0;
         }else{
             motorLForward = 0;
             motorLBackward = 0;
         };
         breakmotor = 0;
-        console.log('L : '+L+ ' R :'+R+' radius : '+ radius + ' theta: '+ theta + ' LF='+ motorLForward +' LB='+motorLBackward+' RF='+ motorRForward+' RB='+motorRBackward);
+        //console.log('motorLForward='+ motorLForward +' motorLBackward='+motorLBackward+' motorRForward='+ motorRForward+' motorRBackward='+motorRBackward);
         TRex.writeBytes(0x0F, [motorLForward, motorLBackward,motorRForward,motorRBackward,breakmotor], function(err) { if(err){console.log("i2c Error: "+ err);} });       
 
     });
@@ -158,8 +104,8 @@ io.sockets.on('connection', function (socket) {
 
 // Read Battery Level 
 // setInterval(function(){
-//     TRex.readBytes(0x0F, 2, function(err, res) {
-         // result contains a buffer of bytes
+//     TRex.readBytes(0x0C, 2, function(err, res) {
+//         // result contains a buffer of bytes
 //         if(err){
 //             console.log("i2c Read battery Error: "+ err);
 //         };   
@@ -167,7 +113,19 @@ io.sockets.on('connection', function (socket) {
 //     }); 
 // }, 10 *1000);
 
+// Production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 console.log('Server Listening on port '+port);
 console.log('Cast Control Interface: http://192.168.10.1:3000');
 console.log('Camera Settings Interface: http://192.168.10.1:8080');
 console.log('ENJOY YOUR RIDE ;) ');
+    
+
